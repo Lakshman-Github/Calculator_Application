264361fb7852612f52f0c1f7098e53a4d3ebd9ce
Lakshman <114009860+Lakshman-Github@users.noreply.github.com>
2024-10-19 14:17:12 +0530

    Add files via upload

---
+++ square_root.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Square_root
+{
+public static void main(String[] args)
+{
+Scanner in = new Scanner(System.in);
+System.out.println("Enter the value of a : ");
+int a = in.nextInt();
+
+int answer = a*a;
+System.out.println("The Added value is : "+answer);
+}
+}
