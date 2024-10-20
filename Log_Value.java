03c765aecd1adb14d14a621f5a0458407f1a6bcd
Lakshman <114009860+Lakshman-Github@users.noreply.github.com>
2024-10-20 11:38:39 +0530

    Add files via upload

---
+++ Log_Value.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Log_Value 
+{
+public static void main(String[] args) 
+{
+
+Scanner in = new Scanner(System.in);
+double number = in.nextDouble();
+System.out.println("Log Value : "+Math.log(number));
+    
+}
+}
